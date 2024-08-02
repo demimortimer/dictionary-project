2c6c5013ad98ceea3dab2b6d97cad25c5be72648
demimortimer <153766264+demimortimer@users.noreply.github.com>
2024-08-02 17:18:16 +0100

    axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,12 +6,11 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
   }
 
   let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=31044e85dc6dotb5a7ff80ae77ab2504`;
