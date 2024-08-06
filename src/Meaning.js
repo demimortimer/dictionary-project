866536ecf47a64827c9bb711227618ef466c1eb4
demimortimer <153766264+demimortimer@users.noreply.github.com>
2024-08-06 21:52:52 +0100

    synonyms added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -6,7 +7,10 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <p>
+        <em>{props.meaning.example}</em>
+      </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
